2173eb79cc8cef11e956f2a5caa4eed4a277ca88
Zyl Pavel <pzyl@willesden.by>
2024-02-02 11:03:06 +0300

    final

--- final.py
+++ final.py
@@ -8,4 +8,11 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
 
+one_hot = pd.DataFrame()
+unique_values = data['whoAmI'].unique()
+for value in unique_values:
+    one_hot[value] = (data['whoAmI'] == value).astype(int)
+
+print(one_hot.head())
+
 
